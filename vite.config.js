de08ba5f96a9970d33b6a107ad1e308efbdec2a0
hanielSceva95 <haniel.sceva@gmail.com>
2026-01-09 15:49:52 -0600

    update repo name in vita.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hanielSceva95.github.io',
+  base: './hanielPortfolio',
   esbuild: {
     loader: 'jsx',
   },
